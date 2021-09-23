b6a4306a026dc279bbbc96e0fca1d5836fda7ba2
DKBARB10 <dkbarb10@localhost.localdomain>
2021-09-23 01:09:35 -0500

    .

--- arimetic_funcs.c
+++ arimetic_funcs.c
@@ -16,14 +16,14 @@ void _mod(stack_t **stack, unsigned int line_number);
  */
 void _add(stack_t **stack, unsigned int line_number)
 {
-    if ((*stack)->next == NULL || (*stack)->next->next == NULL)
-    {
-        set_op_tok_error(short_stack_error(line_number, "add"));
-        return;
-    }
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error(short_stack_error(line_number, "add"));
+		return;
+	}
 
-    (*stack)->next->next->n += (*stack)->next->n;
-    _pop(stack, line_number);
+	(*stack)->next->next->n += (*stack)->next->n;
+	_pop(stack, line_number);
 }
 
 /**
@@ -37,14 +37,14 @@ void _add(stack_t **stack, unsigned int line_number)
  */
 void _sub(stack_t **stack, unsigned int line_number)
 {
-    if ((*stack)->next == NULL || (*stack)->next->next == NULL)
-    {
-        set_op_tok_error(short_stack_error(line_number, "sub"));
-        return;
-    }
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error(short_stack_error(line_number, "sub"));
+		return;
+	}
 
-    (*stack)->next->next->n -= (*stack)->next->n;
-    _pop(stack, line_number);
+	(*stack)->next->next->n -= (*stack)->next->n;
+	_pop(stack, line_number);
 }
 
 /**
@@ -58,20 +58,20 @@ void _sub(stack_t **stack, unsigned int line_number)
  */
 void _div(stack_t **stack, unsigned int line_number)
 {
-    if ((*stack)->next == NULL || (*stack)->next->next == NULL)
-    {
-        set_op_tok_error(short_stack_error(line_number, "div"));
-        return;
-    }
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error(short_stack_error(line_number, "div"));
+		return;
+	}
 
-    if ((*stack)->next->n == 0)
-    {
-        set_op_tok_error(div_error(line_number));
-        return;
-    }
+	if ((*stack)->next->n == 0)
+	{
+		set_op_tok_error(div_error(line_number));
+		return;
+	}
 
-    (*stack)->next->next->n /= (*stack)->next->n;
-    _pop(stack, line_number);
+	(*stack)->next->next->n /= (*stack)->next->n;
+	_pop(stack, line_number);
 }
 
 /**
@@ -85,14 +85,14 @@ void _div(stack_t **stack, unsigned int line_number)
  */
 void _mul(stack_t **stack, unsigned int line_number)
 {
-    if ((*stack)->next == NULL || (*stack)->next->next == NULL)
-    {
-        set_op_tok_error(short_stack_error(line_number, "mul"));
-        return;
-    }
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error(short_stack_error(line_number, "mul"));
+		return;
+	}
 
-    (*stack)->next->next->n *= (*stack)->next->n;
-    _pop(stack, line_number);
+	(*stack)->next->next->n *= (*stack)->next->n;
+	_pop(stack, line_number);
 }
 
 /**
@@ -106,18 +106,18 @@ void _mul(stack_t **stack, unsigned int line_number)
  */
 void _mod(stack_t **stack, unsigned int line_number)
 {
-    if ((*stack)->next == NULL || (*stack)->next->next == NULL)
-    {
-        set_op_tok_error(short_stack_error(line_number, "mod"));
-        return;
-    }
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error(short_stack_error(line_number, "mod"));
+		return;
+	}
 
-    if ((*stack)->next->n == 0)
-    {
-        set_op_tok_error(div_error(line_number));
-        return;
-    }
+	if ((*stack)->next->n == 0)
+	{
+		set_op_tok_error(div_error(line_number));
+		return;
+	}
 
-    (*stack)->next->next->n %= (*stack)->next->n;
-    _pop(stack, line_number);
+	(*stack)->next->next->n %= (*stack)->next->n;
+	_pop(stack, line_number);
 }
